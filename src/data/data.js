10a57ece28c2e8191a0311cdcbd21698e6a48662
Daniel Marin <danielmarin998@hotmail.com>
2022-12-11 19:54:33 -0500

    data changes

---
+++ src/data/data.js
@@ -0,0 +1,159 @@
+import pronosticoIcon from '../assets/imgs/pronostico.svg';
+import conocimientoIcon from '../assets/imgs/conocimiento.svg';
+import suscripcionesIcon from '../assets/imgs/suscripciones.svg';
+import miEmpresaIcon from '../assets/imgs/mi-empresa.svg';
+import misDatosIcon from '../assets/imgs/mis-datos.svg';
+import configuracionIcon from '../assets/imgs/configuracion.svg';
+import newCampaingIcon from '../assets/imgs/newCampaing.svg';
+import editSegmentIcon from '../assets/imgs/editSegments.svg';
+import goToDashboardIcon from '../assets/imgs/goToDashboard.svg';
+
+export const options = [
+  {
+    id: 1,
+    icon: pronosticoIcon,
+    text: 'Pronóstico'
+  },
+  {
+    id: 2,
+    icon: conocimientoIcon,
+    text: 'Conocimiento'
+  },
+  {
+    id: 3,
+    icon: suscripcionesIcon,
+    text: 'Suscripciones'
+  },
+  {
+    id: 4,
+    icon: miEmpresaIcon,
+    text: 'Mi empresa'
+  },
+  {
+    id: 5,
+    icon: misDatosIcon,
+    text: 'Mis datos'
+  },
+  {
+    id: 6,
+    icon: configuracionIcon,
+    text: 'Configurar'
+  }
+];
+
+export const dataBox = [
+  {
+    id: 1,
+    text: '80%',
+    subText: 'Clientes'
+  },
+  {
+    id: 2,
+    text: '10K',
+    subText: 'Ordenes'
+  },
+  {
+    id: 3,
+    text: '200M',
+    subText: 'Monto compras'
+  },
+  {
+    id: 4,
+    text: '5d',
+    subText: 'Monto prom.'
+  }
+];
+
+export const shorcutList = [
+  {
+    id: 1,
+    icon: newCampaingIcon,
+    text: 'Nueva campaña'
+  },
+  {
+    id: 2,
+    icon: editSegmentIcon,
+    text: 'Editar Segmentos'
+  },
+  {
+    id: 3,
+    icon: goToDashboardIcon,
+    text: 'Ir a tablero'
+  }
+];
+
+export const chartData = [
+  {
+    id: 'Dormidos',
+    label: 'Dormidos',
+    value: 89,
+    color: 'hsla(257, 66%, 47%)'
+  },
+  {
+    id: 'Olvidados',
+    label: 'Olvidados',
+    value: 76,
+    color: 'hsla(35, 99%, 64%)'
+  },
+  {
+    id: 'En Riesgo',
+    label: 'En Riesgo',
+    value: 80,
+    color: 'hsla(14, 100%, 61%)'
+  },
+  {
+    id: 'Potenciales',
+    label: 'Potenciales',
+    value: 90,
+    color: 'hsla(110, 46%, 66%)'
+  },
+  {
+    id: 'Campeones',
+    label: 'Campeones',
+    value: 100,
+    color: 'hsla(201, 88%, 42%)'
+  }
+];
+
+export const clientsData = [
+  {
+    id: 1,
+    name: 'Andrea Vasquez'
+  },
+  {
+    id: 2,
+    name: 'Stiwar Gonzales'
+  },
+  {
+    id: 3,
+    name: 'Luis Lopez'
+  },
+  {
+    id: 4,
+    name: 'Clara Zuluaga'
+  },
+  {
+    id: 5,
+    name: 'Nico'
+  },
+  {
+    id: 6,
+    name: 'Samuel Marin'
+  },
+  {
+    id: 7,
+    name: 'Allison Marin'
+  },
+  {
+    id: 8,
+    name: 'Nicol'
+  },
+  {
+    id: 9,
+    name: 'Sam'
+  },
+  {
+    id: 10,
+    name: 'Colby'
+  }
+];
